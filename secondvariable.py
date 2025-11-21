b258dce4ffd741076598093bd0db7e365602e6a3
suhanidhanupur-su <suhanidhanupur@gmail.com>
2025-11-21 19:35:03 +0530

    variable 2

---
+++ secondvariable.py
@@ -0,0 +1,38 @@
+my_variable = 12
+# my_variable is variable and 12 is the value 
+print(my_variable)
+
+name = "Wilmot"
+print(name)
+
+# the task is we have print Wilmot 
+
+
+# which is variable , and which is value .
+
+
+first_name = "Shashant"
+last_name = "Pandey"
+
+value_1 = 13
+# 1_value = 15 
+# print(1_value)
+print(value_1)
+
+print(first_name)
+
+first_student_name  ="WIlmot"
+
+FIRST_student_name = "SHarik"
+
+print(FIRST_student_name)
+
+# else = 88
+# print(else)
+
+s_name = "Wilmot"
+student_name = "Sharik"
+print(student_name)
+
+
+
