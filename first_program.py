9af349a9b6da8ad7496c7b8f597152e01ac59fbf
enter your username <enter your email>
2025-11-29 16:13:20 +0530

    first one

---
+++ first_program.py
@@ -0,0 +1,10 @@
+#🧮 Program 1: Check if number is Positive or Negative
+my_number = int(input("Hii, Ujwal! Enter your favorite number: "))
+
+if my_number > 0:
+    print("Your favorite number is positive.")
+else:
+    print("Your favorite number is negative.")
+
+
+
