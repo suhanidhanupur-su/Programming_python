d63aed383d521293594e7f5cf3b144e210d94160
enter your username <enter your email>
2025-11-29 16:09:39 +0530

    condition

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,4 @@
-my_list = [ 'Ravi', 'Sonu', 'Ziyarat', 'Ujwal' ]
-print("Last element of my_list: ", my_list[-1])
-print("Second last element of my_list: ", my_list[-2])
-print("Third last element of my_list: ", my_list[-3])
-print("Fourth last element of my_list: ", my_list[-4])
-
+if :        
+   print("it will print this line of code  this")
+else:
+   print("it will print this line of code  that")
