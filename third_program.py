781e6ffe63ca8d63d92eca0ffa19bc2d0570bf49
enter your username <enter your email>
2025-11-29 16:29:24 +0530

    third one

---
+++ third_program.py
@@ -0,0 +1,9 @@
+#🗳️ Program 3: Voting Eligibility in Bihar
+my_age = int(input("Hii, suhani! Enter your age: "))
+residence = input("Do you live in Bihar? (yes/no): ").lower()
+
+if my_age >= 18 and residence == 'yes':
+    print("You are eligible to vote in Bihar.")
+else:
+    print("You are not eligible to vote in Bihar.")
+
