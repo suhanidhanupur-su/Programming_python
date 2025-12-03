6109261b28e808acc5d8f66bb8fd3fcb764f1e72
enter your username <enter your email>
2025-12-03 21:21:48 +0530

    added 2 more files

--- arthmethic.py
+++ arthmethic.py
@@ -1,13 +1,6 @@
 x = 10
-y = 3
-print("the output of x + y : ", x + y)  # Addition
-print("the output of x - y : ", x - y)  # Subtraction
-print("the output of x * y : ", x * y)  # Multiplication
-print("Hii, Ravi, You can see the value of x and y : ")  
-print("hii, this is Zeyarat")
-
-print("the output of division : ", x / y)  # Division
-print("the output of floor division : ", x // y)  # Floor Division
-print("the output of modulus : ", x % y)  # Modulus
-
-print("the output of exponentiation : ", 2 ** 4)  # Exponentiation
+y = 5
+print("the sum of x and y is: ", x + y)
+# x  = x + y
+x += y  # x = x + y
+print("the value of x after adding y is: ", x)
